2ee91c54e41ba09b7bd93360df95f7039226ef06
vo0doo <kirsanov.bvt@gmail.com>
2022-10-28 10:05:16 +0300

    feat: create winget_code.js"

---
+++ модули/кредиты/виджеты/root_css_debug/winget_code.js
@@ -0,0 +1,13 @@
+<% if (Context.data.debug_enable) { %>
+    <style>
+    * { background-color: rgba(255,0,0,.2); }
+    * * { background-color: rgba(0,255,0,.2); }
+    * * * { background-color: rgba(0,0,255,.2); }
+    * * * * { background-color: rgba(255,0,255,.2); }
+    * * * * * { background-color: rgba(0,255,255,.2); }
+    * * * * * * { background-color: rgba(255,255,0,.2); }
+    * * * * * * * { background-color: rgba(255,0,0,.2); }
+    * * * * * * * * { background-color: rgba(0,255,0,.2); }
+    * * * * * * * * * { background-color: rgba(0,0,255,.2); }
+    </style>
+<% } %>
